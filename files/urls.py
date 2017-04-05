aaa35898cb3e86371aa2a6dadc485f301f17a114
Monika Grabowska <monika@Monikas-MacBook-Pro-2.local>
2017-04-05 08:49:54 -0400

    Created new URL for file metadata.

--- files/urls.py
+++ files/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 
 from .views import MyView
+from . import views 
 
 
 urlpatterns = [
-    url('', MyView.as_view(), name='form'),
+    url(r'^form/', MyView.as_view(), name='form'),
+    url(r'^meta/', views.meta, name='meta'),
 ]
+
+
+
+
+
