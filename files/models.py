6f7bcac6f6b7d7d10d4e9e85ce68492338c136c9
Monika Grabowska <monika@Monikas-MacBook-Pro-2.local>
2017-04-05 08:59:44 -0400

    Updated database structure.

--- files/models.py
+++ files/models.py
@@ -6,14 +6,12 @@ from s3direct.fields import S3DirectField
 class Upload(models.Model):
     custom_filename = S3DirectField(dest='custom_filename', blank=True)
 
-
 class File(models.Model):
     up_file = models.ForeignKey('Upload')
     upload = S3DirectField(dest='misc', blank=True)
 
-class Data(models.Model):
-    title = models.CharField(max_length=128) 
-    author = models.CharField(max_length=128)
-    publisher = models.CharField(max_length=128)
-    publication_date = models.DateField()
-    citations = models.IntegerField()
+class Meta(models.Model):
+    filename = models.CharField(max_length=128)
+    size = models.IntegerField()
+    upload_date = models.DateTimeField()
+
