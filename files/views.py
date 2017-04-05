9a85f86f329c4d02c054713f4d8ddb1148ba5311
Monika Grabowska <monika@Monikas-MacBook-Pro-2.local>
2017-04-05 08:52:03 -0400

    Updated to retrieve file metadata and save to database.

--- files/views.py
+++ files/views.py
@@ -3,7 +3,41 @@ from django.views.generic import FormView
 
 from .forms import S3DirectUploadForm
 
+from django.views.generic import TemplateView
+
+from django.utils import timezone
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Meta
 
 class MyView(FormView):
     template_name = 'form.html'
     form_class = S3DirectUploadForm
+
+@login_required(redirect_field_name='/admin/login/')
+def meta(request):
+    
+    if 'name' in request.POST:
+      name = request.POST['name']
+    
+    if 'size' in request.POST:
+      size = request.POST['size']
+
+      md = Meta.objects.create(
+        filename = name,
+        size = size,
+        upload_date = timezone.now()
+      )
+      md.save()
+      
+      response = {
+        'status': 1,
+        'message': 'Data saved'
+      }
+      return HttpResponse(response, content_type='application/json')
+
+    else:
+      name = False
+      output = ''
+      return HttpResponse(output)
+
