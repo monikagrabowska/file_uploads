09176bc3c457ea36a4385da07465c4a2367eb278
Monika Grabowska <monika@Monikas-MacBook-Pro-2.local>
2017-04-26 11:34:13 -0400

    Added login page and user registration page.

--- file_uploads/urls.py
+++ file_uploads/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views as auth_views
+from files import views
 
 admin.autodiscover()
-
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^s3direct/', include('s3direct.urls')),
-    url(r'', include('files.urls')),
-    #url(r'^form/', include('files.urls')),
+    url(r'', include('files.urls')), 
+    url(r'^login/$', auth_views.login, name='login'),
 ]
+
